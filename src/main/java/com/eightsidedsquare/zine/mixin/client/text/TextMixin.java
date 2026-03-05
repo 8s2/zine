f36c1a1cefc1a469c39c8767b0fbc0ae74148496
EightSidedSquare <contact@eightsidedsquare.com>
2026-03-04 20:02:28 -0500

    Fix text wrapping error

--- src/main/java/com/eightsidedsquare/zine/mixin/client/text/TextMixin.java
+++ src/main/java/com/eightsidedsquare/zine/mixin/client/text/TextMixin.java
@@ -1,6 +1,7 @@
 package com.eightsidedsquare.zine.mixin.client.text;
 
 import com.eightsidedsquare.zine.common.text.ZineText;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
@@ -12,6 +13,9 @@ import java.util.List;
 public interface TextMixin extends ZineText {
     @Override
     default List<Text> zine$wrap(int width) {
+        if(!RenderSystem.isOnRenderThread()) {
+            return ZineText.super.zine$wrap(width);
+        }
         return MinecraftClient.getInstance().textRenderer
                 .wrapLines((Text) this, width)
                 .stream()
